1eecbe8b7c8cf4f7d706ba3c9de1d81a03d7640c
Evan Bonertz <evan.bonertz@commercetools.com>
2016-07-19 17:57:10 -0400

    Change Email

--- app.js
+++ app.js
@@ -3,9 +3,10 @@
 //Solution: Use Node JS to connec to commercetools API to get customer information and print out
 
 //Connect to API Url (http://sphere.io.com/down-under
-//Read The Data from the response
+
 //HTTP Get web request
-//Parse the Data > Convert to CSV
+//Parse the Data
+//Format to CSV for Field Headers
 //Send the Data to the Google Sheets API
 
 
@@ -19,9 +20,22 @@ function printMessage(username, email) {
 var request = http.get("https://api.sphere.io/down-under/customer" , function(response){
   console.dir(response);
   console.log(response.status);
-
+  var body = " ";
+  response.on('data', function(chunk) {
+    console.log('BODY: ' + chunk);
+  });
+  response.on('end', function() {
+    console.log(body);
+  })
 }):
+
+//Set an error for a bad response
 
 request.on("error", function(error){
   console.error(error.message);
+});
+
+//Read The Data from the response
+response.on('data', function(chunk) {
+  console.log('BODY: ' + chunk);
 });
